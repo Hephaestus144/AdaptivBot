f605a4f9f1c98141cf765c73daf93553d4b82e5f
Storm <storm.collins@standardbank.co.za>
2019-03-04 16:10:18 +0200

    FEAT: Attempt to resolve erratic javascript error

--- AdaptivBot/FunctionForms/RiskViewSettings.xaml.cs
+++ AdaptivBot/FunctionForms/RiskViewSettings.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using AutoIt;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -16,12 +15,14 @@ using Application = System.Windows.Application;
 namespace AdaptivBot.SettingForms
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for RiskViewSettings.xaml
     /// </summary>
     public partial class RiskViewSettings : Page
     {
-        private readonly MainWindow window = (MainWindow)Application.Current.MainWindow;
+        private readonly MainWindow _window = (MainWindow)Application.Current.MainWindow;
 
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+       
 
         public RiskViewSettings()
         {
@@ -33,33 +34,26 @@ namespace AdaptivBot.SettingForms
         {
             GlobalConfigValues.Instance.extractionStartTime = DateTime.Now;
 
-            
-            window?.Logger.NewExtraction("Risk View Reports Extraction Started");
+            _window?.Logger.NewExtraction("Risk View Reports Extraction Started");
 
             if (!CredentialStore.Instance.StoreUserCredentials())
             {
                 return;
             }
 
-
             // TODO: Use binding here.
-            var username = window.TxtUserName.Text;
-            var password = window.TxtPasswordBox.Password;
+            var username = _window?.TxtUserName.Text;
+            var password = _window?.TxtPasswordBox.Password;
 
-            var selectedInstruments = new List<string>();
-
-            foreach (var selectedItem in lstBxInstruments.SelectedItems)
-            {
-                selectedInstruments.Add(InstrumentLists.InstrumentGuiNameToFolderNameMapping[
-                    selectedItem.ToString()
-                        .Replace("System.Windows.Controls.ListBoxItem: ", "")]);
-            }
+            var selectedInstruments = (from object selectedItem in lstBxInstruments.SelectedItems
+                select InstrumentLists.InstrumentGuiNameToFolderNameMapping[selectedItem.ToString()
+                    .Replace("System.Windows.Controls.ListBoxItem: ", "")]).ToList();
 
             var instrumentsToLoopOver = (selectedInstruments.Count != 0)
                 ? selectedInstruments
                 : InstrumentLists.InstrumentFolderNameToInstrumentBatchMapping.Keys.ToList();
 
-            var currentAdaptivEnvironment = window.CmbBxAdaptivEnvironments.SelectedValue.ToString();
+            var currentAdaptivEnvironment = _window?.CmbBxAdaptivEnvironments.SelectedValue.ToString();
             var numberOfFailedExtractions = 0;
             var numberOfSuccessfulExtractions = 0;
             foreach (var instrumentBatch in instrumentsToLoopOver)
@@ -68,109 +62,154 @@ namespace AdaptivBot.SettingForms
                 {
                     try
                     {
-                        window.Logger.NewProcess($"{instrumentBatch} risk view extraction started...");
+                        _window.Logger.NewProcess($"{instrumentBatch} risk view extraction started...");
                         await Task.Run(() =>
-                            window.OpenAdaptivAndLogin(username, password,
+                            _window.OpenAdaptivAndLogin(username, password,
                                 currentAdaptivEnvironment));
 
                         #region wait for browser
 
-                        while (!window.completedLoading)
+                        while (!_window.completedLoading)
                         {
                             await Task.Run(() => Thread.Sleep(100));
                         }
 
                         await Task.Run(() => Thread.Sleep(2000));
-                        window.completedLoading = false;
+                        _window.completedLoading = false;
 
                         #endregion wait for browser
 
                         #region wait for browser
 
-                        while (!window.completedLoading)
+                        while (!_window.completedLoading)
                         {
                             await Task.Run(() => Thread.Sleep(100));
                         }
 
                         await Task.Run(() => Thread.Sleep(2000));
-                        window.completedLoading = false;
+                        _window.completedLoading = false;
 
                         #endregion wait for browser
 
-                        window.InjectJavascript(nameof(JsScripts.OpenRiskView),
+                        _window.InjectJavascript(nameof(JsScripts.OpenRiskView),
                             JsScripts.OpenRiskView);
+                        _window.WebBrowser.Document.InvokeScript(nameof(JsScripts.OpenRiskView));
+                        //await Task.Run(() => Thread.Sleep(1000));
+                        //if (AutoItX.WinExists("Script Error") != 0)
+                        //{
+                        //    throw new Exception();
+                        //}
 
-                        await Task.Run(() => Thread.Sleep(1000));
-                        window.WebBrowser.Document?.InvokeScript(nameof(JsScripts.OpenRiskView));
+                        //var cancellationToken = tokenSource.Token;
+                        //var checkForJsError =
+                        await Task.Run(() =>
+                        {
+                            for (var i = 0; i < 10; i++)
+                            {
+                                Thread.Sleep(100);
+                                //cancellationToken.ThrowIfCancellationRequested();
+                                if (AutoItX.WinExists("Script Error") != 0)
+                                {
+                                    Console.Write("Fucking found");
+                                    //throw new Exception();
+                                }
+                            }
+                        });
 
                         #region wait for browser
 
-                        for (int i = 0; i < 3; i++)
+                        for (var i = 0; i < 3; i++)
                         {
-                            while (!window.completedLoading)
+                            while (!_window.completedLoading)
                             {
                                 await Task.Run(() => Thread.Sleep(100));
                             }
-
-                            window.completedLoading = false;
+                            _window.completedLoading = false;
                         }
 
                         await Task.Run(() => Thread.Sleep(2000));
 
                         #endregion wait for browser
 
-                        window.Logger.OkayText = $"Filtering for {instrumentBatch}...";
-                        window.InjectJavascript(
+                        _window.Logger.OkayText = $"Filtering for {instrumentBatch}...";
+                        _window.InjectJavascript(
                             nameof(JsScripts.FilterRiskViewOnInstruments),
                             JsScripts.FilterRiskViewOnInstruments);
-                        window.WebBrowser.Document.InvokeScript(
+                        _window.WebBrowser.Document.InvokeScript(
                             nameof(JsScripts.FilterRiskViewOnInstruments),
                             new object[] { InstrumentLists.InstrumentFolderNameToInstrumentBatchMapping[instrumentBatch] });
 
+                        await Task.Run(() =>
+                        {
+                            for (var i = 0; i < 10; i++)
+                            {
+                                Thread.Sleep(100);
+                                //cancellationToken.ThrowIfCancellationRequested();
+                                if (AutoItX.WinExists("Script Error") != 0)
+                                {
+                                    Console.Write("Fucking found");
+                                    //throw new Exception();
+                                }
+                            }
+                        });
+
 
                         #region wait for browser
 
-                        while (!window.completedLoading)
+                        while (!_window.completedLoading)
                         {
                             await Task.Run(() => Thread.Sleep(100));
                         }
 
                         await Task.Run(() => Thread.Sleep(1000));
-                        window.completedLoading = false;
+                        _window.completedLoading = false;
 
                         #endregion wait for browser
 
 
-                        window.InjectJavascript(nameof(JsScripts.ExportToCsv),
+                        _window.InjectJavascript(nameof(JsScripts.ExportToCsv),
                             JsScripts.ExportToCsv);
-                        window.WebBrowser.Document.InvokeScript(nameof(JsScripts.ExportToCsv));
-
-                        await Task.Run(() => Thread.Sleep(500));
+                        _window.WebBrowser.Document.InvokeScript(nameof(JsScripts.ExportToCsv));
+                        await Task.Run(() =>
+                        {
+                            for (var i = 0; i < 10; i++)
+                            {
+                                Thread.Sleep(100);
+                                //cancellationToken.ThrowIfCancellationRequested();
+                                if (AutoItX.WinExists("Script Error") != 0)
+                                {
+                                    Console.Write("Fucking found");
+                                    //throw new Exception();
+                                }
+                            }
+                        });
 
                         #region wait for browser
 
-                        while (!window.completedLoading)
+                        while (!_window.completedLoading)
                         {
                             await Task.Run(() => Thread.Sleep(100));
                         }
 
                         await Task.Run(() => Thread.Sleep(1000));
-                        window.completedLoading = false;
+                        _window.completedLoading = false;
 
                         #endregion wait for browser
 
-                        while (window.WebBrowser.Document.GetElementsByTagName("A").Count == 0)
+                        while (_window.WebBrowser.Document.GetElementsByTagName("A").Count == 0)
                         {
                             await Task.Run(() => Thread.Sleep(100));
                         }
 
-                        foreach (HtmlElement link in window.WebBrowser.Document
+                        foreach (HtmlElement link in _window.WebBrowser.Document
                             .GetElementsByTagName("A"))
                         {
                             if (link.InnerText.Equals("exported file link"))
                                 link.InvokeMember("Click");
                         }
 
+                        tokenSource.Cancel();
+                        Thread.Sleep(1000);
                         var overrideExistingFile = (bool)chkBxOverrideExistingFiles.IsChecked;
                         await Task.Run(() =>
                             SaveFile(instrumentBatch, overrideExistingFile));
@@ -181,37 +220,37 @@ namespace AdaptivBot.SettingForms
                     {
                         if (errorCount < 2)
                         {
-                            window.Logger.ErrorText = $"Something failed for {instrumentBatch} extraction. Trying again. Attempt number: {++errorCount}";
+                            _window.Logger.ErrorText = $"Something failed for {instrumentBatch} extraction. Trying again. Attempt number: {++errorCount}";
                         }
                         else
                         {
                             numberOfFailedExtractions++;
-                            window.Logger.ErrorText = $"{instrumentBatch} extraction failed 3 times. Moving on to next instrument set.";
+                            _window.Logger.ErrorText = $"{instrumentBatch} extraction failed 3 times. Moving on to next instrument set.";
                         }
                     }
                 }
             }
 
-            window.Logger.ExtractionComplete("Risk View Extraction");
-            window.Logger.OkayTextWithoutTime =
+            _window.Logger.ExtractionComplete("Risk View Extraction");
+            _window.Logger.OkayTextWithoutTime =
                 $"Number of successful extractions: {numberOfSuccessfulExtractions}";
             if (numberOfFailedExtractions > 0)
             {
-                window.Logger.ErrorTextWithoutTime =
+                _window.Logger.ErrorTextWithoutTime =
                     $"Number of failed extractions: {numberOfFailedExtractions}";
             }
             else
             {
-                window.Logger.OkayTextWithoutTime =
+                _window.Logger.OkayTextWithoutTime =
                     $"Number of failed extractions: {numberOfFailedExtractions}";
             }
 
             GlobalConfigValues.Instance.extractionEndTime = DateTime.Now;
             var timeSpan = GlobalConfigValues.Instance.extractionEndTime -
                            GlobalConfigValues.Instance.extractionStartTime;
-            window.Logger.OkayTextWithoutTime
+            _window.Logger.OkayTextWithoutTime
                 = $"Extraction took: {timeSpan.Minutes} minutes {timeSpan.Seconds % 60} seconds";
-            window.WebBrowser.Url = new Uri("C:\\GitLab\\AdaptivBot\\ExtractionComplete.html");
+            _window.WebBrowser.Url = new Uri("C:\\GitLab\\AdaptivBot\\ExtractionComplete.html");
         }
 
 
@@ -223,7 +262,7 @@ namespace AdaptivBot.SettingForms
             AutoItX.Send("{ENTER}");
             Dispatcher.Invoke((System.Action)(() =>
             {
-                window.Logger.OkayText = $"Saving CSV file for {instrumentBatch}...";
+                _window.Logger.OkayText = $"Saving CSV file for {instrumentBatch}...";
             }));
 
             AutoItX.WinWait("Save As", timeout: 20);
@@ -249,7 +288,7 @@ namespace AdaptivBot.SettingForms
                     AutoItX.Send("!y");
                     Dispatcher.Invoke((System.Action)(() =>
                     {
-                        window.Logger.WarningText =
+                        _window.Logger.WarningText =
                             $"Overriding existing file for {instrumentBatch}...";
                     }));
                 }
@@ -258,7 +297,7 @@ namespace AdaptivBot.SettingForms
                     AutoItX.Send("!n");
                     Dispatcher.Invoke((System.Action)(() =>
                     {
-                        window.Logger.WarningText =
+                        _window.Logger.WarningText =
                             $"File already exists for {instrumentBatch}.";
                     }));
                     AutoItX.WinWait("Save As", timeout: 20);
@@ -292,7 +331,7 @@ namespace AdaptivBot.SettingForms
                     : $"{(new FileInfo(filePath).Length / 1024):n}" + " KB";
                 Dispatcher.Invoke((System.Action)(() =>
                 {
-                    window.extractedFiles.Add(new ExtractedFile()
+                    _window.extractedFiles.Add(new ExtractedFile()
                     {
                         FilePath = filePath,
                         FileName = Path.GetFileName(filePath),
@@ -327,7 +366,7 @@ namespace AdaptivBot.SettingForms
                 txtBoxBaseFolder.Text;
 
             document.Save(GlobalDataBindingValues.Instance.AdaptivBotConfigFilePath);
-            window.Logger.OkayText = "Risk view settings saved.";
+            _window.Logger.OkayText = "Risk view settings saved.";
         }
     }
 }
