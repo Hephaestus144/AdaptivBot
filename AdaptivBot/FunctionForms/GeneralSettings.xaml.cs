6d9889253e5f747662dca6becf3b9e68e2ef8af1
Storm <storm.collins@standardbank.co.za>
2019-04-02 22:19:38 +0200

    FEAT: Added functionality to delete config file

--- AdaptivBot/FunctionForms/GeneralSettings.xaml.cs
+++ AdaptivBot/FunctionForms/GeneralSettings.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -48,6 +49,20 @@ namespace AdaptivBot.SettingForms
         private void BtnPurgeAdaptivCredentials_Click(object sender, RoutedEventArgs e)
         {
             CredentialStore.Instance.PurgeCredentials();
+        }
+
+        private void BtnDeleteAdaptivBotConfigFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(GlobalDataBindingValues.Instance.AdaptivBotConfigFilePath))
+            {
+                File.Delete(GlobalDataBindingValues.Instance.AdaptivBotConfigFilePath);
+                _window.Logger.WarningText = "Config file deleted!";
+                _window.Logger.WarningText = "Beware that this significantly impacts functionality.";
+            }
+            else
+            {
+                _window.Logger.WarningText = "Config file not found!";
+            }
         }
     }
 }
